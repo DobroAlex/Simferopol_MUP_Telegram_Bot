fdd57bbacd0acefa2c4597b65f183f53770d123f
Alex <alexkoshernosiegov@gmail.com>
2019-09-25 23:24:20 +0300

    Fixed DB & collection names

--- MUP_TG_BOT/models/user_model.py
+++ MUP_TG_BOT/models/user_model.py
@@ -2,7 +2,7 @@ import pymongo
 
 client = pymongo.MongoClient()
 print(f'Mongo is connected at  {client.address}')
-db = client.MUP_Users
-collection = db.MUP_Users
+db = client.MUP_User
+collection = db.MUP_User
 
 
