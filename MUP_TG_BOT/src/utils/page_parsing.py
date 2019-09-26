ac49828c1122a585883e16ad5ad273975cadb6a8
Alex <alexkoshernosiegov@gmail.com>
2019-09-26 14:08:59 +0300

    Fixed invalid page detection

--- MUP_TG_BOT/src/utils/page_parsing.py
+++ MUP_TG_BOT/src/utils/page_parsing.py
@@ -6,6 +6,6 @@ def is_valid_page(addr):
     content = urllib.request.urlopen(addr).read().decode('utf-8')
     soup = BeautifulSoup(content, features='lxml')
     soup_result = soup.find('div', attrs={'class': 'nachislenija_div'})
-    if not soup_result:
-        return True
-    return False
+    if 'Вы неточно ввели' in soup_result.text:
+        return False
+    return True
